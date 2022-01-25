0493cdb1cbe69d4a339f5da72a209bee48d73c84
Yago Pajariño <yago977@hotmail.com>
2022-01-25 01:03:55 +0100

    adds menu layout and style

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,5 @@
 /* eslint-disable eqeqeq */
-export default function menu() {
+export default function contenedormenu() {
   const oldDiv = document.querySelector(".container");
   if (oldDiv != undefined) {
     oldDiv.remove();
@@ -13,5 +13,37 @@ export default function menu() {
   divTitle.textContent = "Menu desplegable";
   newDiv.appendChild(divTitle);
 
+  const contenedor = document.createElement("div");
+  const title = document.createElement("h2");
+  title.classList.toggle("menuTitle");
+  title.textContent = `Expandir ${String.fromCodePoint(0x1f447)}`;
+
+  contenedor.appendChild(title);
+  contenedor.classList.toggle("contentDiv");
+  const optionsDiv = document.createElement("div");
+  optionsDiv.classList.toggle("optionsList");
+  optionsDiv.style.display = "none";
+  const prs = ["Nam gravida dolor vel elit semper placerat", "Praesent ultricies tortor at leo pulvinar, id egestas ligula volutpat.", "Donec sagittis ante nec lorem suscipit sodales.", "Curabitur blandit ex vel lobortis aliquam."];
+  prs.forEach((element) => {
+    const span = document.createElement("span");
+    span.textContent = element;
+    optionsDiv.appendChild(span);
+  });
+
+  title.addEventListener("click", () => {
+    const div = document.querySelector(".optionsList");
+    const text = document.querySelector(".menuTitle");
+    const value = div.style.display;
+    if (value == "flex") {
+      div.style.display = "none";
+      text.textContent = `Expandir ${String.fromCodePoint(0x1f447)}`;
+    } else {
+      div.style.display = "flex";
+      text.textContent = `Contraer ${String.fromCodePoint(0x1f446)}`;
+    }
+  });
+
+  contenedor.appendChild(optionsDiv);
+  newDiv.appendChild(contenedor);
   body.appendChild(newDiv);
 }
