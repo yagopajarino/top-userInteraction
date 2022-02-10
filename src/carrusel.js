c56144a3e3df32ab74a401177734f326cf46e4d3
Yago Pajariño <yago977@hotmail.com>
2022-02-10 19:20:40 -0300

    new linter

--- src/carrusel.js
+++ src/carrusel.js
@@ -1,37 +1,37 @@
 /* eslint-disable eqeqeq */
-import img1 from "./img1.jpeg";
-import img2 from "./img2.jpeg";
-import img3 from "./img3.jpeg";
-import img4 from "./img4.jpeg";
-import img0 from "./img5.jpeg";
+import img1 from './img1.jpeg';
+import img2 from './img2.jpeg';
+import img3 from './img3.jpeg';
+import img4 from './img4.jpeg';
+import img0 from './img5.jpeg';
 
 export default function carrusel() {
-  const oldDiv = document.querySelector(".container");
+  const oldDiv = document.querySelector('.container');
   if (oldDiv != undefined) {
     oldDiv.remove();
   }
 
   const images = [img1, img2, img3, img4, img0];
 
-  const body = document.querySelector("body");
+  const body = document.querySelector('body');
 
-  const newDiv = document.createElement("div");
-  newDiv.classList.toggle("container");
-  const divTitle = document.createElement("h1");
-  divTitle.textContent = "Carrusel de imagenes";
+  const newDiv = document.createElement('div');
+  newDiv.classList.toggle('container');
+  const divTitle = document.createElement('h1');
+  divTitle.textContent = 'Carrusel de imagenes';
 
-  const imgDiv = document.createElement("div");
-  imgDiv.classList.add("imgDiv");
-  const prevArrow = document.createElement("div");
-  const nextArrow = document.createElement("div");
-  prevArrow.classList.toggle("imgArrow");
-  nextArrow.classList.toggle("imgArrow");
+  const imgDiv = document.createElement('div');
+  imgDiv.classList.add('imgDiv');
+  const prevArrow = document.createElement('div');
+  const nextArrow = document.createElement('div');
+  prevArrow.classList.toggle('imgArrow');
+  nextArrow.classList.toggle('imgArrow');
 
   imgDiv.appendChild(prevArrow);
 
   images.forEach((element) => {
-    const i = document.createElement("img");
-    i.classList.add("slideImg");
+    const i = document.createElement('img');
+    i.classList.add('slideImg');
     i.src = element;
     imgDiv.appendChild(i);
   });
@@ -41,8 +41,8 @@ export default function carrusel() {
   newDiv.appendChild(divTitle);
   newDiv.appendChild(imgDiv);
 
-  const p = document.createElement("p");
-  p.textContent = "TODO: add arrows and timer to autoslide";
+  const p = document.createElement('p');
+  p.textContent = 'TODO: add arrows and timer to autoslide';
 
   body.appendChild(newDiv);
   body.appendChild(p);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import "./style.css";
-import menu from "./menu";
-import carrusel from "./carrusel";
+import './style.css';
+import menu from './menu';
+import carrusel from './carrusel';
 
-const body = document.querySelector("body");
-const buttonsDiv = document.createElement("div");
-buttonsDiv.classList.toggle("buttonsDiv");
+const body = document.querySelector('body');
+const buttonsDiv = document.createElement('div');
+buttonsDiv.classList.toggle('buttonsDiv');
 
-const carrButton = document.createElement("div");
-carrButton.classList.toggle("shifterButton");
-carrButton.textContent = "Carrusel";
+const carrButton = document.createElement('div');
+carrButton.classList.toggle('shifterButton');
+carrButton.textContent = 'Carrusel';
 
-const menuButton = document.createElement("div");
-menuButton.classList.toggle("shifterButton");
-menuButton.textContent = "Menu desplegable";
+const menuButton = document.createElement('div');
+menuButton.classList.toggle('shifterButton');
+menuButton.textContent = 'Menu desplegable';
 
 buttonsDiv.appendChild(carrButton);
 buttonsDiv.appendChild(menuButton);
 
 body.appendChild(buttonsDiv);
 
-carrButton.addEventListener("click", carrusel);
-menuButton.addEventListener("click", menu);
+carrButton.addEventListener('click', carrusel);
+menuButton.addEventListener('click', menu);
 
-console.log("here");
+console.log('here');
